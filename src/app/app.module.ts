1f0a2a996c435665fa37ba5bb33ae72f5e8b568e
jviilaa <javior@floridauniversitaria.es>
2021-02-08 12:18:21 +0100

    RB-Genres component created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeViewComponent } from './home-view/home-view.component';
+import { GenresViewComponent } from './genres-view/genres-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeViewComponent
+    HomeViewComponent,
+    GenresViewComponent
   ],
   imports: [
     BrowserModule,
